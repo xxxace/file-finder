aafc3418f09825714adf7984f50a41b1f6152fb6
xxxace <731162160@qq.com>
2022-09-13 13:59:55 +0800

    remove hevueImgPreview

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import hevueImgPreview from 'hevue-img-preview';
 // import './samples/node-api'
 
 createApp(App)
-  .use(hevueImgPreview)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
