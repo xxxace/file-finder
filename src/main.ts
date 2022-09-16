b6f386c9fecabec69c56955dd257d68659828307
xxxace <731162160@qq.com>
2022-09-16 15:12:06 +0800

    add: fileIconVectors to replace useFileTypeIcon

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './utils/flexible'
+import 'file-icon-vectors/dist/file-icon-classic.min.css';
+import 'file-icon-vectors/dist/file-icon-vectors.min.css';
 // import './samples/node-api'
 
 createApp(App)
