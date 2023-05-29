95f88acc1d5a74d6e10649a176000ca6b1285efb
xxxace <731162160@qq.com>
2023-05-29 15:14:26 +0800

    add NConfigProvider for global setting

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <n-loading-bar-provider>
-    <FileFinder />
-  </n-loading-bar-provider>
+  <n-config-provider :locale="locale" :date-locale="dateLocale">
+    <n-loading-bar-provider>
+      <FileFinder />
+    </n-loading-bar-provider>
+  </n-config-provider>
 </template>
 
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { ref } from 'vue';
 import FileFinder from './views/FileFinder/index.vue';
-import { NLoadingBarProvider } from 'naive-ui';
+import { NConfigProvider, NLoadingBarProvider, zhCN, dateZhCN } from 'naive-ui';
+import type { NLocale, NDateLocale } from 'naive-ui';
 
+const locale = ref<NLocale | null>(zhCN);
+const dateLocale = ref<NDateLocale | null>(dateZhCN);
 </script>
 
 <style>
