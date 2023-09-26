bbaf2d749f003c80223c074d671ae6866586baa2
xxxace <731162160@qq.com>
2023-09-26 15:41:19 +0800

    change: alter useNotify code and add NMessageProvider, NNotificationProvider in APP.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <n-config-provider :locale="locale" :date-locale="dateLocale">
-    <n-loading-bar-provider>
-      <FileFinder />
-    </n-loading-bar-provider>
+    <n-notification-provider>
+      <n-message-provider>
+        <n-loading-bar-provider>
+          <FileFinder />
+        </n-loading-bar-provider>
+      </n-message-provider>
+    </n-notification-provider>
   </n-config-provider>
 </template>
 
@@ -11,7 +15,7 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import { ref } from 'vue';
 import FileFinder from './views/FileFinder/index.vue';
-import { NConfigProvider, NLoadingBarProvider, zhCN, dateZhCN } from 'naive-ui';
+import { NConfigProvider, NLoadingBarProvider, NMessageProvider, NNotificationProvider, zhCN, dateZhCN } from 'naive-ui';
 import type { NLocale, NDateLocale } from 'naive-ui';
 
 const locale = ref<NLocale | null>(zhCN);
